4570e00549831457b430e1d0f73942647abf684e
John Ngundeng <john.ngundeng@ocsbstudent.ca>
2018-03-01 10:46:09 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,15 @@
+-- Title: MovingImages
+-- Name: John Ngundeng
+-- Course: ICS2O/3C
+-- This program displays an bettleship moing across the screen. As well as another
+--  object moving in a different direction 
+
+
+-- hide the status bar 
+display.setStatusBar(display.HiddenStatusBar)
+
+-- global variables 
+scrollSpeed = 3 
+
+-- background image with width and height
+local backgroundImage = 
